a8d9df52807e773d1e7d4723c060cab37fc52711
neoestod <sean.tong1459@gmail.com>
2013-05-22 16:52:18 -0400

    10modify

--- 10_multiplyNum.cpp
+++ 10_multiplyNum.cpp
@@ -3,7 +3,7 @@ void array_multiplication(int A[], int OUTPUT[], int n) {
   int left = 1;
   int right = 1;
   for (int i = 0; i < n; i++)
-	OUTPUT[i] = 1;
+    OUTPUT[i] = 1;
   for (int i = 0; i < n; i++) {
     OUTPUT[i] *= left;
     OUTPUT[n - 1 - i] *= right;
